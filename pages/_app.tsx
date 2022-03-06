ac0016df62a0d2e1ab64a9eda79cd523f8af66fa
EthanBurkett <ethan@ethanburkett.net>
2022-03-06 00:13:06 -0600

    Added meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   const theme = useTheme();
   return (
     <>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>BlueLight Studios</title>
       <Particles
